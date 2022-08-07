e5227fe78d0e0a2a1d36d6ef1d4b7a13e4a1ebaf
MaStEr <masternonnolnw@gmail.com>
2022-08-07 17:27:10 +0700

    account-exists-with-different-credential handle

--- module/Signin/components/SigninBox/components/ErrorLoginHandle.ts
+++ module/Signin/components/SigninBox/components/ErrorLoginHandle.ts
@@ -4,6 +4,8 @@ export function SignInErrorHandle(message: string) {
   else if (message.indexOf('wrong-password') != -1) return 'Wrong password!'
   else if (message.indexOf('too-many-requests') != -1)
     return 'Too many requests try again later or reset your password.'
+  else if (message.indexOf('account-exists-with-different-credential') != -1)
+    return 'You already use this Email with another account.'
   else {
     console.log(message)
     return 'Unknown error'
